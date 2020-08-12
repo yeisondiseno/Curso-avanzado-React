194850ce1f10ca4ffc59dbfa022050d543bd87a2
Yeison Montoya <yeisondiseno@gmail.com>
2020-08-11 22:35:49 -0500

    primeas imágenes y estructura

---
+++ c-avanzado-react/src/components/Badge.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Badge extends React.Component {
+    render(){
+        return(
+            <div>
+                <div>
+                    <img src="" alt="Logo de la conferencia" />
+                </div>
+                <div>
+                    <img src="" alt="Avatar" />
+                    <h1>
+                        Richard 
+                        <br/>
+                        Kaufman
+                    </h1>
+                </div>
+                <div>
+                    <p>
+                        Frontend Engenieer
+                    </p>
+                    <p>
+                        @sparragus
+                    </p>
+                </div>
+                <div>
+                    #platziconf
+                </div>
+            </div>
+        )
+    }
+}
+ 
+
+
+export default Badge; 
